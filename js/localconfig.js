3fae3b8980ceacc986ce5a50d2a17e9ebdb5c1a3
xun <mx@rmbz.net>
2016-01-24 22:24:11 +0800

    css

---
+++ js/localconfig.js
@@ -0,0 +1,13 @@
+define(function () {
+    return {
+        backend: "http://127.0.0.1:3001/",
+        'shake': {
+            'interval': 2000,
+            'period': 20
+        },
+        'wall': {
+            'interval': 2000,
+            'count': 5
+        }
+    };
+});
