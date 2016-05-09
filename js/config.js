8270c78e195952a2530694906388265cbc537287
xun <mx@rmbz.net>
2016-05-09 22:53:23 +0800

    2015年会更新

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 define(['localconfig'], function (localconfig) {
 
     var defaultSettings = {
-        'backend': 'http://192.168.1.187:3001/',  // 用于指定后端服务器路径， 可以是"/api/", 或者"http://<ip>:<port>/api/", 必须以'/'结尾
+        'backend': '/',  // 用于指定后端服务器路径， 可以是"/api/", 或者"http://<ip>:<port>/api/", 必须以'/'结尾
         'shake': {
             'interval': 500, //结果刷新间隔
             'period': 20 // 摇一摇游戏时间
